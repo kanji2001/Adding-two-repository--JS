135263d9de7997afb01d93e90724b2231b640d31
kanji2001 <153625398+kanji2001@users.noreply.github.com>
2024-01-19 15:05:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addNumbers() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const sum = num1 + num2;
+    document.getElementById('result').innerText = `Answer is: ${sum}`;
+}
